76f3d279cfd1c6527d1693f7010898e5dd6e92af
Artem Konevskikh <artem.konevskih@gmail.com>
2024-07-01 08:32:43 +0200

    z dim shape fixed

--- interpolation_grid.py
+++ interpolation_grid.py
@@ -106,16 +106,19 @@ def interpolation_grid(
     os.makedirs(outdir, exist_ok=True)
     label = torch.zeros([1, G.c_dim], device=device)
     np_zs = [np.random.RandomState(seed).randn(1, G.z_dim) for seed in seeds]
+    
     first_row_zs = np.zeros((num_steps_x+1, G.z_dim))
     last_row_zs = np.zeros((num_steps_x+1, G.z_dim))
     grid_zs = np.zeros(
         (num_steps_y+1, first_row_zs.shape[0], first_row_zs.shape[1])
     )
 
+
     for x in range(num_steps_x+1):
         alpha = x / num_steps_x
         first_row_zs[x] = interpolate_arrays(np_zs[0], np_zs[1], alpha)
         last_row_zs[x] = interpolate_arrays(np_zs[2], np_zs[3], alpha)
+        
     for y in range(num_steps_y+1):
         alpha_y = y / num_steps_y
         grid_zs[y] = interpolate_arrays(
@@ -123,10 +126,9 @@ def interpolation_grid(
             last_row_zs,
             alpha_y
         )
-
     for y, row in enumerate(grid_zs):
         for x, z in enumerate(row):
-            z = torch.from_numpy(z).to(device)
+            z = torch.from_numpy(z.reshape(1, 512)).to(device)
             img = G(z, label, truncation_psi=truncation_psi,
                     noise_mode=noise_mode)
             img = (
@@ -137,34 +139,6 @@ def interpolation_grid(
             ).save(
                 f'{outdir}/frame{x:04d}_{y:04d}.png'
             )
-
-# def interpolation_grid():
-#     array1 = np.array([1, 2, 3])
-#     array2 = np.array([4, 5, 6])
-#     array3 = np.array([7, 8, 9])
-#     array4 = np.array([10, 11, 12])
-
-#     num_steps_x = 10
-#     num_step_y = 10
-
-#     top_row = np.zeros((num_steps_x+1, array1.shape[0]))
-#     bottom_row = np.zeros((num_steps_x+1, array1.shape[0]))
-#     grid = np.zeros((num_step_y+1, top_row.shape[0], top_row.shape[1]))
-
-#     for x in range(num_steps_x+1):
-#         alpha = x / num_steps_x
-#         top_row[x] = interpolate_arrays(array1, array2, alpha)
-#         bottom_row[x] = interpolate_arrays(array3, array4, alpha)
-#     for y in range(num_step_y+1):
-#         alpha_y = y / num_step_y
-#         grid[y] = interpolate_arrays(
-#             top_row,
-#             bottom_row,
-#             alpha_y
-#         )
-#     # print(top_row, bottom_row)
-#     print(grid)
-
 
 if __name__ == "__main__":
     interpolation_grid()
